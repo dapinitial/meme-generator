75bf9b247363783901c0e6dd7d6821c27d322e13
David Puerto <me@davidpuerto.com>
2017-09-04 19:02:39 -0700

    style app, app-button, and setting a memeLimit and allowing the user to add ten more at a time

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      memeLimit: 10
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div className="App">
         <h2>Welcome to the meme generator</h2>
         {
-          this.props.memes.map((meme, index) => {
+          this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
             return (
               <h4 key={index}>{meme.name}</h4>
             )
           })
         }
-      </div>
+        <div
+          className="App-button"
+          onClick={() => {
+            this.setState({ memeLimit: this.state.memeLimit + 10 })
+          }}
+        >Load 10 more memes...</div>
+      </div >
     )
   }
 }
