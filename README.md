b8ebe3924bfdd364fe6c2a314a4f7ac7523117e1
David Puerto <me@davidpuerto.com>
2017-09-04 17:24:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+# MEME GENERATOR
+
+Scaffolding out an example Redux application
---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to the meme generator</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
